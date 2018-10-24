df0810261230648897c09a6de38ae63269bc1794
Plo <448950706@qq.com>
2018-10-24 09:50:47 +0800

    all

---
+++ JACardViewDemo/JACardViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JACardViewDemo
+//
+//  Created by Ja on 2018/10/24.
+//  Copyright © 2018年 Ja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
